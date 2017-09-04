96437957037213994ba38cc63fe9a14a31ac5215
walisonjose <walison.josededeus@gmail.com>
2017-09-04 11:19:37 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/deploy-key
-eval `ssh-agent -s` #start shh agent 
-ssh-add ~/.ssh/deploy-key
-chmod 600 config.txt
-mv config.txt ~/.ssh/config
+#chmod 600 deploy-key
+#mv deploy-key ~/.ssh/deploy-key
+#eval `ssh-agent -s` #start shh agent 
+#ssh-add ~/.ssh/deploy-key
+#chmod 600 config.txt
+#mv config.txt ~/.ssh/config
+
+
+cat deploy-key >> ~/.ssh/authorized_keys
