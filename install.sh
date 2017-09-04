cef549633f5f553a28e263038406bc44c111590c
walisonjose <walison.josededeus@gmail.com>
2017-09-03 22:51:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
-nmap medialab.ufg.br
+chmod 600 deploy-key
+mv deploy-key ~/.ssh/deploy-key
+eval `ssh-agent -s` #start shh agent 
+ssh-add ~/.ssh/deploy-key
+chmod 600 config.txt
+mv config.txt ~/.ssh/config
