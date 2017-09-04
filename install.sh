e5c07f18aa0fe7c0cd60aa5d29051bdffe70e5be
walisonjose <walison.josededeus@gmail.com>
2017-09-04 14:42:55 -0300

    Agora Vai!

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ ssh-add ~/.ssh/deploy_key
 #chmod 600 config.txt
 #mv config.txt ~/.ssh/config
 
+pwd
+ls
 
-cat deploy_key >> ~/.ssh/authorized_keys
+#cat deploy_key >> ~/.ssh/authorized_keys
 #cd  ~/.ssh 
 #cat deploy_key >> authorized_keys
 #chmod 600 deploy_key
 #mv deploy_key ~/.ssh/id_rsa
 
-ssh l3p@medialab.ufg.br
+#ssh l3p@medialab.ufg.br
