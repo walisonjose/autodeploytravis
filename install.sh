861514c02ddb610856e1ee55dc0a03510e26eda8
walisonjose <walison.josededeus@gmail.com>
2017-09-04 14:35:44 -0300

    Agora Vai!

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-set -x # Show the output of the following commands (useful for debugging)
-#chmod 600 deploy-key
-#mv deploy-key ~/.ssh/deploy-key
+set -x #Show the output of the following commands (useful for debugging)
+chmod 600 deploy_key
+mv deploy_key ~/.ssh/deploy_key
 eval `ssh-agent -s` #start shh agent 
-#ssh-add ~/.ssh/deploy-key
+ssh-add ~/.ssh/deploy_key
 #chmod 600 config.txt
 #mv config.txt ~/.ssh/config
 
 
-#cat deploy-key >> ~/.ssh/authorized_keys
-
-chmod 600 deploy_key
-mv deploy_key ~/.ssh/id_rsa
+cat deploy_key >> ~/.ssh/authorized_keys
+#cd  ~/.ssh 
+#cat deploy_key >> authorized_keys
+#chmod 600 deploy_key
+#mv deploy_key ~/.ssh/id_rsa
 
 ssh l3p@medialab.ufg.br
