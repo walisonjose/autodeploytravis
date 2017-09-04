bb6b09ff908c347f4f8a8d32aa4cc6464f76243d
walisonjose <walison.josededeus@gmail.com>
2017-09-04 15:42:14 -0300

    Agora Vai!

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 set -x
  #Show the output of the following commands (useful for debugging)
 chmod 600 deploy_key
+cat no.txt
+
+
 #mv deploy_key ~/.ssh/deploy_key
-cat deploy_key >> ~/.ssh/authorized_keys
+#cat deploy_key >> ~/.ssh/authorized_keys
 #eval `ssh-agent -s` #start shh agent 
 #ssh-add ~/.ssh/deploy_key
 #chmod 600 config.txt
@@ -15,4 +18,4 @@ cat deploy_key >> ~/.ssh/authorized_keys
 #chmod 600 deploy_key
 #mv deploy_key ~/.ssh/id_rsa
 
-ssh l3p@medialab.ufg.br uname -a
+#ssh l3p@medialab.ufg.br uname -a
